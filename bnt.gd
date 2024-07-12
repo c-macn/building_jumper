71849c3f26e843e7a3f64e17db59b620fcc5c2e0
Cliodhna <chris.mcnulty@tutanota.com>
2024-07-12 15:21:11 +0100

    wtf

--- bnt.gd
+++ bnt.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 class_name BlackAndTan
 
-func _physics_process(delta) -> void:
-	velocity.y -= 1.0;
-	move_and_collide(velocity * delta)
+# func _physics_process(delta) -> void:
+# 	velocity.y -= 1.0;
+# 	move_and_collide(velocity * delta)
 
--- gerry.gd
+++ gerry.gd
@@ -1,15 +1,15 @@
 extends CharacterBody3D
 class_name Gerry
 
+@onready var path_follow: PathFollow3D = get_parent();
+
 var bullet = preload('res://pew.tscn')
 
 @onready var bullet_spawner = $BulletSpawner
 
 func _physics_process(delta) -> void:
-	velocity.y -= 1.0;
-	var h_move = Input.get_axis("move_left", "move_right")
-	velocity.x = h_move * 10.0
-	move_and_collide(velocity * delta)
+	if path_follow != null:
+		path_follow.set_progress(path_follow.get_progress() + 2.5 * delta)
 
 	if Input.is_action_just_pressed('ui_accept'):
 		var pew = bullet.instantiate()
